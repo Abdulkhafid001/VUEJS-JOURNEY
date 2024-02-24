ff329ad02bbd934746ce275334b04dcfc80e4e02
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2024-02-25 00:41:22 +0400

    display data

--- VUE_APPS/MYBLOG/views.py
+++ VUE_APPS/MYBLOG/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
+from MYBLOG.models import UserDetails
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('userName')
+        pass_word = request.POST.get('passWord')
+        # save the details to the userDetails database
+        user = UserDetails(user_name=user_name, pass_word=pass_word)
+        # save the details
+        user.save()
+    mydata = UserDetails.objects.values_list('user_name')
     template = loader.get_template('login.html')
+    context = {'users': mydata}
+    # print(user_name, pass_word)
     return HttpResponse(template.render())
