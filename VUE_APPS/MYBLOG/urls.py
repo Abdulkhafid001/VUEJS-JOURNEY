a335b055ee29e6dc8ded636f857f844c1ef9fa2a
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2024-02-29 17:41:51 +0400

    fixing login

--- VUE_APPS/MYBLOG/urls.py
+++ VUE_APPS/MYBLOG/urls.py
@@ -1,6 +1,11 @@
 # get views to map to url paths
 from django.urls import path
 from MYBLOG import views
+# using the Django auth views function
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.django_auth, name='signupview'),
+    path('login', views.django_login, name='loginview'),
+    path('home/', views.home, name='homepage'),
+    path('logout/', views.home, name='logout'),
 ]
--- VUE_APPS/MYBLOG/views.py
+++ VUE_APPS/MYBLOG/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 from MYBLOG.models import UserDetails
 
@@ -21,3 +23,35 @@ def django_auth(request):
     my_users = User.objects.all().values()
     context = {'myUsers': my_users, }
     return render(request, "signup.html", context=context)
+
+# Django log in using GET http method
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def logout(request):
+    logout(request)
+    return render(request, "signup.html")
+
+# use GET for websearch not password retrieve
+
+
+def django_login(request):
+    if request.method == 'POST':
+        user_name = request.POST['userName']
+        user_pass_word = request.POST['passWord']
+        print(user_name, user_pass_word)
+        user_email = request.GET.get('userEmail')
+        # check if user is authenticated and redirect to homepage
+        user = authenticate(request, username=user_name,
+                            password=user_pass_word)
+        # if user is found redirect to homepage
+        if user is not None:
+            # log user in
+            login(request, user)
+            return redirect(home)
+        else:
+            return HttpResponse("incorrect username or password. try again")
+    return render(request, "login.html")
